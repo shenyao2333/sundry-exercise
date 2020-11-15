49bf7feef7b8e74c24953477ab7e80defe19299e
sy91aa <sy91aa@163.com>
2020-11-15 23:26:06 +0800

    first commit

---
+++ ex-easyexcel/src/main/java/com/sy/sunder/ex/easyexcel/controller/ExcelController.java
@@ -0,0 +1,25 @@
+package com.sy.sunder.ex.easyexcel.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: shenyao
+ * @Date: Created by 2020/11/15 23:24
+ * @description:
+ */
+@RestController
+@RequestMapping("/excel")
+public class ExcelController {
+
+
+
+    @GetMapping("/in")
+    public void  sdf(){
+
+    }
+
+
+
+}
