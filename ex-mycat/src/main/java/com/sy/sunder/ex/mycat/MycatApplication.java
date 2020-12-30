e271b1fca3f1a05d935ba804cb56c18500a362bb
sy91aa <sy91aa@163.com>
2020-12-30 22:21:33 +0800

    1、添加mycat模块，准备学习分库分表

---
+++ ex-mycat/src/main/java/com/sy/sunder/ex/mycat/MycatApplication.java
@@ -0,0 +1,20 @@
+package com.sy.sunder.ex.mycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: shenyao
+ * @Date: Created by 2020/12/30 22:18
+ * @description: 启动类
+ */
+@SpringBootApplication
+public class MycatApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycatApplication.class, args);
+    }
+
+
+}
