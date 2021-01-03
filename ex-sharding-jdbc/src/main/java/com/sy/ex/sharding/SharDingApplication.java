abed50122bc228cacfc279ff6a47b5f9fe1783d4
sy91aa <sy91aa@163.com>
2021-01-03 22:05:30 +0800

    1、最终选用shrdingjdbc最分库分表吧

---
+++ ex-sharding-jdbc/src/main/java/com/sy/ex/sharding/SharDingApplication.java
@@ -0,0 +1,18 @@
+package com.sy.ex.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: shenyao
+ * @Date: Created by 2021/1/3 18:32
+ * @description:
+ */
+@SpringBootApplication
+public class SharDingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharDingApplication.class, args);
+    }
+
+}
