c64160f95a6e6144333803260f1983810eca6403
shenyao2333 <shenyao2333@163.com>
2021-06-04 16:21:49 +0800

    1、动态获取用户

---
+++ ex-camunda/src/main/java/com/sy/sunder/camunda/compose/CandidateUser.java
@@ -0,0 +1,20 @@
+package com.sy.sunder.camunda.compose;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: sy
+ * @Date: Created by 2021/6/4 10:17
+ * @description:
+ */
+@Component
+public class CandidateUser {
+
+
+
+    public String getUser(){
+        return "zhangsan";
+    }
+
+
+}
