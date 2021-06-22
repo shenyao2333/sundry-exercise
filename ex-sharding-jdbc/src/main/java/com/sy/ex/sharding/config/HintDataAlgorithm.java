1c9285763489dbf3e95ffb46c40a7460ef60b29e
shenyao2333 <shenyao2333@163.com>
2021-06-22 11:18:37 +0800

    1、sharding-jdbc基本完成，等待文档完善

--- ex-sharding-jdbc/src/main/java/com/sy/ex/sharding/config/HintDataAlgorithm.java
+++ ex-sharding-jdbc/src/main/java/com/sy/ex/sharding/config/HintDataAlgorithm.java
@@ -4,7 +4,9 @@ import org.apache.shardingsphere.api.sharding.hint.HintShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.hint.HintShardingValue;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @Author: sy
@@ -17,8 +19,16 @@ public class HintDataAlgorithm implements HintShardingAlgorithm {
 
     @Override
     public Collection<String> doSharding(Collection collection, HintShardingValue hintShardingValue) {
-        System.out.println(" hintShardingValue "+ hintShardingValue);
-        System.out.println("collection  "+ collection);
-        return null;
+        List<String> shardingResult = new ArrayList<>();
+        System.out.println(" 选库条件： "+ hintShardingValue);
+        Collection values = hintShardingValue.getValues();
+        for (Object value : values) {
+            for (Object value2 : collection) {
+                if (value2.toString().endsWith(value.toString())){
+                    shardingResult.add(value2.toString());
+                }
+            }
+        }
+        return shardingResult;
     }
 }
--- ex-sharding-jdbc/src/main/java/com/sy/ex/sharding/config/HintTableAlgorithm.java
+++ ex-sharding-jdbc/src/main/java/com/sy/ex/sharding/config/HintTableAlgorithm.java
@@ -4,21 +4,37 @@ import org.apache.shardingsphere.api.sharding.hint.HintShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.hint.HintShardingValue;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @Author: sy
  * @Date: Created by 2021/6/21 16:37
- * @description:
+ * @description: 不再从SQL 解析中获取值，而是直接通过hintManager.addTableShardingValue("t_order", 1)参数指定
  */
 @Component
 public class HintTableAlgorithm  implements HintShardingAlgorithm {
 
 
     @Override
-    public Collection<String> doSharding(Collection collection, HintShardingValue hintShardingValue) {
-        System.out.println(" hintShardingValue "+ hintShardingValue);
-        System.out.println("collection  "+ collection);
-        return null;
+    public Collection<String> doSharding(Collection availableTargetNames, HintShardingValue shardingValue) {
+        System.out.println("选表条件=" + shardingValue);
+
+        List<String> shardingResult = new ArrayList<>();
+        for (Object targetName : availableTargetNames) {
+            // hint分片算法的ShardingValue有两种具体类型:
+            // ListShardingValue和RangeShardingValue
+            // 使用哪种取决于HintManager.addDatabaseShardingValue(String, String, ShardingOperator,...),ShardingOperator的类型
+            Collection<Object> values = shardingValue.getValues();
+            for (Object value : values) {
+                if (targetName.toString().endsWith(value.toString())){
+                    shardingResult.add(targetName.toString());
+                }
+            }
+        }
+        System.out.println(shardingResult);
+        return shardingResult;
+
     }
 }
